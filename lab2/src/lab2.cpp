dedef661ed4a2931c41311d2f9551f1bcb9ed13c
deepikasingh19 <singh.deepika2005@gmail.com>
2016-03-19 15:40:58 +0530

    Added files via upload

--- lab2/src/lab2.cpp
+++ lab2/src/lab2.cpp
@@ -1,116 +1,166 @@
-//============================================================================
-// Name        : lab2.cpp
-// Author      : 
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
-
-//============================================================================
-// Name        : lab2.cpp
-// Author      :
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
-
-#include <iostream>
+#include<iostream>
+using namespace std;
 #include<fstream>
 #include<sstream>
+#include<string>
 using namespace std;
-
 class student
 {
-	string usn,name,branch;
-	int sem;
-public:
-	void pack();
-	void read();
-	void write();
-	void unpack();
-	void search(int);
-	void delet();
-	void modify();
-};
-fstream f1;
-void student::read()
-{
-	cout<<"Enter the usn:"<<endl;
-	cin>>usn;
-	cout<<"Enter the name:"<<endl;
-	cin>>name;
-	cout<<"Enter the sem:"<<endl;
-		cin>>sem;
-
-	cout<<"Enter the branch:"<<endl;
-	cin>>branch;
-}
-void student::pack()
-{
-	string buffer,sem1;
-	int l;
-	stringstream ss;
-				ss<<sem;
-				 sem1=ss.str();
-
-	buffer=usn+"|"+name+"|"+sem1+"|"+branch;
-	buffer.resize(100,'$');
-	f1<<buffer<<endl;
-}
-
-void student::unpack()
-{
-	string extra;
-	f1.open("file.txt",ios::in);
-	getline(f1,usn,'|');
-	getline(f1,name,'|');
-	getline(f1,branch,'|');
-	f1.close();
-}
-void student::search(string key)
-{
-int f;
-	f1.open("file.txt",ios::in);
-	while(!f1.eof())
-	{
-		getline(f1,buffer);
-		unpack();
-		if(usn==key)
-		{ f=1;
-		  cout<<"success"
-		}
-		else
-		{
-			cout<<"not found."<<endl;
-		}
-	}
-}
+ public:
+     string usn,name,branch,buffer;
+   int semester;
+  void read();
+   void pack();
+  void write();
+  void unpack();
+  int search(string);
+ int del(string);
+ void modify(string);
+ };
 int main()
 {
-	int ch,key;
-	student s;
-	while(1)
-	{
-	cout<<"Enter your choice:"<<endl;
-	cin>>ch;
-	switch(ch)
-	{
-	case 1:s.read();
-	f1.open("file.txt",ios::out|ios::app);
-		if(!f1)
-		{
-			cout<<"File not opened."<<endl;
-		}
-		s.pack();
-		f1.close();
-	        break;
-	case 2: cout<<"Enter the key:"<<endl;
-	        cin>>key;
-	        s.search(key);
-	        break;
-	default: cout<<"Wrong choice."<<endl;
-	         exit(0);
-	}
-	}
-	return 0;
+ int count,choice,len,i;
+ student s;
+  string key;
+  cout<<"\n1.add record\n2.delete record\n3.modify\n4.search\n";
+  cin>>choice;
+  switch(choice)
+{
+  case 1:cout<<"how many record?\n";
+         cin>>count;
+          for(i=0;i<count;i++)
+           {
+              cout<<"data";
+              s.read();
+              s.pack();
+            }
+          s.write();
+         break;
+   case 2:cout<<"enter the key\n";
+          cin>>key;
+          i=s.del(key);
+          break;
+   case 3:cout<<"enter the key\n";
+          cin>>key;
+           s.modify(key);
+          break;
+   case 4:cout<<"enter the usn\n";
+cin>>key;
+         i=s.search(key);
+          break;
+   default:cout<<"error";
+   }
+ return 0;
+}
+void student::read()
+{
+ cout<<"usn";
+cin>>usn;
+cout<<"name";
+cin>>name;
+cout<<"branch";
+cin>>branch;
+cout<<"semester";
+cin>>semester;
+}
+ void student::pack()
+{
+ string sem,temp;
+ stringstream out;
+ out<<semester;
+ sem=out.str();
+  temp.erase();
+ temp+=usn+'|'+name+'|'+branch+'|'+sem;
+  for(;temp.size()<100;)
+   {
+ temp+='$';
+ }
+ buffer+=temp+'\n';
+}
+
+void student::write()
+{
+ fstream file;
+ file.open("2a.txt",ios::out|ios::app);
+ file<<buffer;
+ file.close();
+}
+ int student::search(string key)
+ {
+ fstream file;
+ int flag=0,pos=0;
+ file.open("2a.txt",ios::in);
+ while(!file.eof())
+{
+ buffer.erase();
+ getline(file,buffer);
+ unpack();
+ if(key==usn)
+{
+ flag=1;
+ cout<<"found the record is:\n"<<buffer;
+ pos=file.tellg();
+ cout<<"\npos="<<pos;
+ return pos;
+ }
+ }
+if(!flag)
+  cout<<"record not found";
+ return pos;
+}
+ void student::unpack()
+{
+ string sem;
+ int ch=1,i=0;
+ usn.erase();
+ while(buffer[i]!='|')
+ usn+=buffer[i++];
+
+}
+int student::del(string key)
+ {
+ fstream file;
+ char del_mark='*',t;
+ int pos=0,flag=0;
+ pos=search(key);
+ if(pos)
+{
+ file.open("2a.txt");
+  pos-=101;
+file.seekp(pos,ios::beg);
+file.put(del_mark);
+flag=1;
+}
+file.close();
+if(!flag) return 0;
+ else
+ return 1;
+}
+ void student::modify(string key)
+{
+ int choice;
+ if(del(key))
+{
+ cout<<"want to modify?\n1.usn 2.name 3.branch 4.semester\nenter u r choice\n";
+ cin>>choice;
+ switch(choice)
+{
+ case 1:cout<<"usn";
+        cin>>usn;
+         break;
+ case 2:cout<<"name";
+        cin>>name;
+ break;
+  case 3:cout<<"branch";
+         cin>>branch;
+         break;
+ case 4:cout<<"semester";
+        cin>>semester;
+ break;
+ default:cout<<"wrong choice";
+}
+ buffer.erase();
+ pack();
+ write();
+}
 }
